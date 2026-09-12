fb96cca4a7202b2f921b056798b3a678bccf5265
tiffany-reset <affiliate0315@gmail.com>
2026-09-12 12:34:13 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "tiffany-reset-v8";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon.svg"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
